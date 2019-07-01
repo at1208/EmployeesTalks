e3ac1980cbd34f19945accfffb30aaf0751049ad
aman tiwari <amantiwari12081999@gmail.com>
2019-07-01 15:47:08 +0530

    initial setup

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './header.css'
+
+const Header = () => {
+  return <div>
+            <div className='a1 container-fluid shadow text-center'>
+             <h1>Canteen</h1>
+            </div>
+            <div className='a2 container shadow text-center'>
+             <h3>Inside Talks</h3>
+            </div>
+        </div>
+}
+export default Header;
